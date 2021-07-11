65cf57a84a9063d5cee3471e2bb3dc8a52e84f5c
handryamatma <handryamatma18@gmail.com>
2021-07-11 22:13:37 +0530

    initite app

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+class StorePicker extends React.Component {
+ render () {
+     return (
+        <Fragment>
+            start
+        </Fragment>
+     )
+ }
+}
+
+export default StorePicker
